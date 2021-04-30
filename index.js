474d8a7eb64dd4fbdb7a191194033061b18eeb28
Michael <mawwoodley@gmail.com>
2021-04-29 21:20:33 -0400

    comment out content type CORS error

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ button.addEventListener("click", grabJoke);
 function grabJoke(){
     const jokeRequest = fetch('https://icanhazdadjoke.com/', {
         headers: {
-           // 'Content-Type': 'application/json',
+            //'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
     })
     console.log(jokeRequest)
 }
+
+
+
+
+
+
+
+
+
+
 
 // Access to fetch at 'https://icanhazdadjoke.com/' from origin 'http://127.0.0.1:5500' has been blocked by CORS policy:
 // Request header field content-type is not allowed by Access-Control-Allow-Headers in preflight response.
