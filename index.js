2716475d70e289ebf15d38ab77bd9ff7cc073ea7
Michael <mawwoodley@gmail.com>
2021-04-29 21:17:24 -0400

    add id to button tag to fix event listener

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const button = document.getElementById('button');
-
-
+const button = document.getElementById("myBtn");
 
 button.addEventListener("click", grabJoke);
 
 function grabJoke(){
-    const jokeText = fetch('https://icanhazdadjoke.com/', {
+    const jokeRequest = fetch('https://icanhazdadjoke.com/', {
         headers: {
-            'Content-Type': 'application/json',
+           // 'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
     })
+    console.log(jokeRequest)
 }
+
+// Access to fetch at 'https://icanhazdadjoke.com/' from origin 'http://127.0.0.1:5500' has been blocked by CORS policy:
+// Request header field content-type is not allowed by Access-Control-Allow-Headers in preflight response.
+// index.js:4 GET https://icanhazdadjoke.com/ net::ERR_FAILED
+// grabJoke @ index.js:4
+// index.html:1 Uncaught (in promise) TypeError: Failed to fetch
