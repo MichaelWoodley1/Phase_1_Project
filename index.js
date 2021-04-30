1cf8f49ffb8baae26277ddcf2811e9e6ce141438
Michael <mawwoodley@gmail.com>
2021-04-29 21:03:02 -0400

    create fetch

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const button = document.getElementById('button');
 button.addEventListener("click", grabJoke);
 
 function grabJoke(){
-    
+    const jokeText = fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
 }
